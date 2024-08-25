f0559d9bc07dcad15c9631596a4cc02564ef38e7
chicoumbetov <shyngysmaaan@gmail.com>
2024-08-25 14:55:44 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+
+module.exports = {
+  ontent: [
+    "./App.{js,jsx,ts,tsx}",
+    "./<custom directory>/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
