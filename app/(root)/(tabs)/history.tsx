3b70490ccc779cabcd7038e7c18f44709ec05997
chicoumbetov <shyngysmaaan@gmail.com>
2024-09-19 18:51:05 +0200

    :sparkles: Bottom Tabs done

--- app/(root)/(tabs)/history.tsx
+++ app/(root)/(tabs)/history.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { SafeAreaView, Text } from "react-native";
 
 const History = () => {
-  return <div>History</div>;
+  return (
+    <SafeAreaView>
+      <Text>Profile</Text>
+    </SafeAreaView>
+  );
 };
 
 export default History;
