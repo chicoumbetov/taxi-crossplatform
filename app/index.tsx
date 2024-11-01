468c141a3b061800f05c5c4094c90da7e2685d89
chicoumbetov <shyngysmaaan@gmail.com>
2024-11-01 11:09:55 +0100

    :bug: fix tailwind web style

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,11 @@
 import { useAuth } from "@clerk/clerk-expo";
 import { Redirect } from "expo-router";
+import { NativeWindStyleSheet } from "nativewind";
 import "react-native-get-random-values";
+
+NativeWindStyleSheet.setOutput({
+  default: "native",
+});
 
 const Page = () => {
   const { isSignedIn } = useAuth();
