8c2f9619d4dafe0d4234cfdde475c4cfbed1a465
chicoumbetov <shyngysmaaan@gmail.com>
2024-11-02 18:45:21 +0100

    :sparkles: stripe native

---
+++ providers/stripe/stripe.native.tsx
@@ -0,0 +1,23 @@
+import * as Stripe from "@stripe/stripe-react-native";
+import {
+  initPaymentSheet,
+  presentPaymentSheet,
+} from "@stripe/stripe-react-native";
+
+export const StripeProvider: React.FC<{
+  publishableKey: string;
+  merchantIdentifier: string;
+  children: JSX.Element | JSX.Element[];
+}> = ({ publishableKey, merchantIdentifier, children }) => {
+  return (
+    <Stripe.StripeProvider
+      merchantIdentifier={merchantIdentifier}
+      publishableKey={publishableKey}
+      urlScheme="myapp"
+    >
+      {children}
+    </Stripe.StripeProvider>
+  );
+};
+
+export { initPaymentSheet, presentPaymentSheet };
